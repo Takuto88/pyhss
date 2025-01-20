87a101c92499742f6324bfb3664f9efc74c88e48
Alexander Couzens <lynxis@fe80.eu>
2025-01-20 10:37:02 +0100

    fixup corenetwork

---
+++ lib/common.py
@@ -0,0 +1,24 @@
+"""
+    PyHSS Common class
+    Copyright (C) 2025  Lennart Rosam <hello@takuto.de>
+    Copyright (C) 2025  Alexander Couzens <lynxis@fe80.eu>
+
+    SPDX-License-Identifier: AGPL-3.0-or-later
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+class CoreNetworkDomain(Enum):
+    PS = 1
+    CS = 2
